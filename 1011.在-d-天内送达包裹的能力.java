b3ca82e0ed8427fd46b0cf420f6df71573979706
MinSC <294879521@qq.com>
2022-08-31 23:49:06 +0800

    feat:常规栈的代码#skip

---
+++ 1011.在-d-天内送达包裹的能力.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode.cn id=1011 lang=java
+ *
+ * [1011] 在 D 天内送达包裹的能力
+ */
+
+// @lc code=start
+class Solution {
+    public int shipWithinDays(int[] weights, int days) {
+
+    }
+}
+// @lc code=end
+
