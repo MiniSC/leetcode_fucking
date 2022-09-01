05683bae7a1aa11c72de70856a7e6bf1e2487565
MinSC <294879521@qq.com>
2022-09-01 15:52:44 +0800

    feat:常规栈的代码#skip

--- 946.验证栈序列.java
+++ 946.验证栈序列.java
@@ -1,3 +1,6 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 /*
  * @lc app=leetcode.cn id=946 lang=java
  *
@@ -7,8 +10,18 @@
 // @lc code=start
 class Solution {
     public boolean validateStackSequences(int[] pushed, int[] popped) {
-
+        Deque<Integer> stack = new ArrayDeque<Integer>();
+        int n = pushed.length;
+        for (int i = 0, j = 0; i < n; i++) {
+            stack.push(pushed[i]);
+            while (!stack.isEmpty() && stack.peek() == popped[j]) {
+                stack.pop();
+                j++;
+            }
+        }
+        return stack.isEmpty();
     }
 }
+
 // @lc code=end
 
